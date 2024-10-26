dd2fe3b739af61137fc63e1868c6c055cfc856c4
BrickiBulli <schmutz.seth@BBZBL-it.ch>
2024-10-26 20:27:27 +0200

    logic for SortierenNachVerbrauch added

--- src/main/java/com/example/m323/funktionen/SortierenNachVerbrauch.java
+++ src/main/java/com/example/m323/funktionen/SortierenNachVerbrauch.java
@@ -1,5 +1,13 @@
 package com.example.m323.funktionen;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.example.m323.Main;
+import com.example.m323.utils.data.DataSet;
+
 /**
  * This class represents an EXAMPLE and contains two methods: iterativeFunction
  * and functionalFunction.
@@ -17,8 +25,28 @@ public class SortierenNachVerbrauch {
      * @author Seth Schmutz
      */
     public static void iterativeFunction() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'iterativeFunction'");
+        List<Integer> uniqueYears = new ArrayList<>();
+        Map<Integer, List<DataSet>> dataByYear = new HashMap<>();
+
+        for (DataSet data : Main.getDataLoader().getData()) {
+            int year = data.getJahr();
+            if (!uniqueYears.contains(year)) {
+                uniqueYears.add(year);
+            }
+            dataByYear.computeIfAbsent(year, k -> new ArrayList<>()).add(data);
+        }
+
+        for (Integer year : uniqueYears) {
+            System.out.println("Year " + year + ":");
+
+            List<DataSet> dataList = dataByYear.get(year);
+            dataList.sort((a, b) -> Double.compare(b.getWert(), a.getWert()));
+
+            for (DataSet data : dataList) {
+                System.out.println("Gemeinde " + data.getGemeinde() + ": " + data.getWert());
+            }
+            System.out.println();
+        }
     }
 
     /**
