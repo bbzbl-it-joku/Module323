6177866b04d5cc649cc0a65131e7e40d070c5f3d
BrickiBulli <schmutz.seth@BBZBL-it.ch>
2024-10-26 20:15:40 +0200

    Added last two Classes for the 2 Methods

---
+++ src/main/java/com/example/m323/funktionen/Verbrauchsstatistik.java
@@ -0,0 +1,34 @@
+package com.example.m323.funktionen;
+
+/**
+ * This class represents an EXAMPLE and contains two methods: iterativeFunction
+ * and functionalFunction.
+ * 
+ * @author Joshua Kunz
+ * @author Seth Schmutz
+ * @version 1.0
+ * @since 10.10.2024
+ */
+public class Verbrauchsstatistik {
+    /**
+     * This method represents an iterative function.
+     * It is currently unimplemented and throws an UnsupportedOperationException.
+     * 
+     * @author Seth Schmutz
+     */
+    public static void iterativeFunction() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'iterativeFunction'");
+    }
+
+    /**
+     * This method represents a functional function.
+     * It is currently unimplemented and throws an UnsupportedOperationException.
+     * 
+     * @author Joshua Kunz
+     */
+    public void functionalFunction() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'functionalFunction'");
+    }
+}
